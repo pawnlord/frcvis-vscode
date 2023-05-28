f7aab4d6bcab888f12b41883ad0989092430b3fc
pawnlord <scruffduff321@gmail.com>
2023-05-28 17:42:40 -0400

    Add buildable plugin project for eclipse

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('frcvis.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('frcvis.hello', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from frcvis!');
 	});
 
 	context.subscriptions.push(disposable);
-}
 
+}
 // This method is called when your extension is deactivated
 export function deactivate() {}
