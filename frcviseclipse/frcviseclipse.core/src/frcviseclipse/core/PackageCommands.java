31866b61ea9a03953364d83d506ff3afdd25bd8b
pawnlord <scruffduff321@gmail.com>
2023-07-08 21:29:49 -0400

    Get type bindings

--- frcviseclipse/frcviseclipse.core/src/frcviseclipse/core/PackageCommands.java
+++ frcviseclipse/frcviseclipse.core/src/frcviseclipse/core/PackageCommands.java
@@ -8,11 +8,15 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.WorkingCopyOwner;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.NodeFinder;
+import org.eclipse.jdt.core.dom.WildcardType;
+import org.eclipse.osgi.internal.loader.EquinoxClassLoader;
 
 import frcviseclipse.core.util.Logger;
 import org.eclipse.jdt.ls.core.internal.JDTUtils;
+import org.eclipse.jdt.ls.core.internal.handlers.CodeActionHandler;
 
 public class PackageCommands {
     public static String CMD_LOG = "frcvis.log";
@@ -28,18 +32,22 @@ public class PackageCommands {
         int offset = offsetReceived.intValue();
         Logger.logOnVSCode("Received: " + uri + ":" + offset);
         ICompilationUnit compilationUnit = JDTUtils.resolveCompilationUnit(uri);
-        WorkingCopyOwner workingCopy = compilationUnit.getOwner();
-        CompilationUnit root;
-        try{
-            root = compilationUnit.reconcile(AST.JLS_Latest, true, false,  workingCopy, monitor);
-        } catch(JavaModelException e){
-            Logger.errorOnVSCode("displayTree: JavaModelException: " + e.getMessage() + "\n Trace: " + e.getStackTrace());
-            return;
-        }
+        
+        CompilationUnit root = CodeActionHandler.getASTRoot(compilationUnit, monitor);
 
         NodeFinder finder = new NodeFinder(root, offset, 1);
-        Logger.logOnVSCode(Integer.toString(finder.getCoveringNode().getNodeType()));
-
+        ASTNode node = finder.getCoveringNode();
+        ASTNode rootNode = finder.getCoveringNode().getRoot();
+        while(node != rootNode){
+            int nodeType = node.getNodeType();
+            Logger.logOnVSCode(node.toString() + ": " + Integer.toString(nodeType));
+            if(nodeType == 14){
+                var type = ((ClassInstanceCreation)node).getType();
+                Logger.logOnVSCode("Type of Class Creation: " + type.toString());
+                Logger.logOnVSCode(" ---- Binding: " + type.resolveBinding().toString());
+            }
+            node = node.getParent();
+        }
 
     }
 }
